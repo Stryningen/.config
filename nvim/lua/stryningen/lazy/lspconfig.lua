3f9dcaad76067d1fd91d02197dd468252774b9d2
Stryningen <fredrsan@gmail.com>
2026-01-05 20:06:27 +0100

    django lsp

--- nvim/lua/stryningen/lazy/lspconfig.lua
+++ nvim/lua/stryningen/lazy/lspconfig.lua
@@ -211,6 +211,7 @@ return {
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
 			-- clangd = {},
+			djls = {},
 			-- gopls = {},
 			pyright = {},
 			-- rust_analyzer = {},
